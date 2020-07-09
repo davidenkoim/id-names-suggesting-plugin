4313751e0272ea0ea6f9961d16db8a99ec82a583
GitHub Action <action@github.com>
2020-07-09 12:45:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/davidenkoim/idnamessuggestingplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/davidenkoim/idnamessuggestingplugin/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.davidenkoim.idnamessuggestingplugin.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.davidenkoim.idnamessuggestingplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/davidenkoim/idnamessuggestingplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.davidenkoim.idnamessuggestingplugin.services
+
+import com.github.davidenkoim.idnamessuggestingplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
